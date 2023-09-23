44ce18c655e432f50070c658bddc26db0175a782
nathangartlan2 <72713800+nathangartlan2@users.noreply.github.com>
2023-09-23 14:20:06 -0500

    saving post preview

--- urbanoutdoorsman/ClientApp/src/components/PostPreview.js
+++ urbanoutdoorsman/ClientApp/src/components/PostPreview.js
@@ -1,40 +1,35 @@
-import React from 'react'
-import styles from '../css/PostPreview.module.css'
+import React from "react";
+import styles from "../css/PostPreview.module.css";
 
+const PostPreview = ({ id, title, author, text, image }) => {
+  const TEXT_PREVIEW_LENGTH = 40;
 
+  const appendText = (text) => {
+    const arrayOfWords = text.split(" ");
+    let sliceArray = arrayOfWords.slice(0, TEXT_PREVIEW_LENGTH);
 
-const PostPreview = ({id, title, author, text, image}) =>
-{
+    let sliced = "";
+    sliceArray.forEach(function(element, index, array) {
+      sliced += `${element} `;
+    });
 
-    const TEXT_PREVIEW_LENGTH = 40;
+    //remove trailing space on end
+    sliced = sliced.slice(0, -1);
 
-    const appendText = (text) => {
-        const arrayOfWords = text.split(" ");
-        let sliceArray = arrayOfWords.slice(0, TEXT_PREVIEW_LENGTH);
+    sliced += "...";
+    return sliced;
+  };
 
-        let sliced = "";
-        sliceArray.forEach(function (element, index, array) {
-            sliced += `${element} `;
-          });
-
-        //remove trailing space on end
-        sliced = sliced.slice(0, -1);
-
-
-        sliced += "...";
-        return sliced
-    }
-
-    return (
+  return (
     <div className={styles.component}>
-        <div className={styles.postCopy}>
-            <h1>{title}</h1>
-            <h5>{author}</h5>
-            <body>{appendText(text)}</body>
-        </div>
-        <img className={styles.img} src={image}></img>
-
-    </div>);
+      <div className={styles.postCopy}>
+        <h1>{title}</h1>
+        <h5>{author}</h5>
+        <body>{appendText(text)}</body>
+      </div>
+      <img className={styles.img} src={image}></img>
+    </div>
+  );
 };
 
 export default PostPreview;
